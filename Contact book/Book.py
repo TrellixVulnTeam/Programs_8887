3c3e45a4314e45328ebda61d4cfd687a3e188728
Aiaksers <mrrdogerman@gmail.com>
2021-01-03 16:34:13 +0300

    v4

--- Contact book/Book.py
+++ Contact book/Book.py
@@ -1,27 +1,48 @@
 class ContactBook:
     print('''
-        Используйте x = ContactBook() для создания книги, где x - название этой книги.
-        Используйте x.comands для просмора комманд.''')
+        Используйте название = ContactBook() для создания книги.
+        Используйте название.comands для просмора комманд.''')
 
     def comands(self):
-        print('''
-        Используйте x.seeallcontact() для просмотра всех контактов.
 
-        Используйте x.addcontact() для добавления контакта.
-        В скобках укажите имя, адрес, номер телефона, адрес электронной почты.
-        Внимание! Все значения, указанные в скобках, должны быть в ковычках!''')
+        print('''
+
+        Используйте название.seeAllcontacts() для просмотра всех контактов.
+
+        Используйте название.addnewContact('имя','адрес','номер телефона','эл. почта') для добавления контакта.
+        Внимание! Все значения, указанные в скобках, должны быть в кавычках!
+
+        Используйте название.redactcontact(порядковый номер контакта,порядковый номер данных,'новые данные')
+        для редактироания ваших контактов.
+
+        Используйте название.deletecontact(Порядковый номер контакта), чтобы удалить контакт.
+
+        Используйте название.clearcontactbook(), чтобы полностью очистить книгу
+        ''')
 
     def __init__(self):
         self.book = []
 
-    def addcontact(self, name, address, phonenumber, mail):
+    def addnewContact(self, name, address, phonenumber, mail):
         self.book.append([name, address, phonenumber, mail])
         print('''
      Ваш контакт был сохранен под номером''', len(self.book))
 
-    def seeallcontact(self):
-        for i in range(len(self.book)):
-            print(i + 1, end='. ')
-            for j in range(len(self.book[i])):
-                print(self.book[i][j])
+    def seeAllcontacts(self):
+        if self.book == []:
+            print('Контакты не обнаружены')
+        else:
+            for i in range(len(self.book)):
+                print(i + 1, end='. ')
+                for j in range(len(self.book[i])):
+                    print(self.book[i][j])
 
+    def redactcontact(self, contactNumber, propertyNumber, NewProperty):
+        self.book[contactNumber - 1][propertyNumber - 1] = NewProperty
+
+    def deletecontact(self, contactNumber):
+        del self.book[contactNumber - 1]
+
+    def clearContactbook(self):
+        for i in range(len(self.book)):
+            del self.book[-1]
