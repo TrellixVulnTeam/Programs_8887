78d8cfdcaffcea146d384c3e1398eda73ef3b53e
Aiaksers <mrrdogerman@gmail.com>
2021-01-30 17:11:36 +0800

    v4

--- Contact book/Book.py
+++ Contact book/Book.py
@@ -1,4 +1,5 @@
 import sqlite3
+import argparse
 
 connectionObject = sqlite3.connect('ContactBookDatabase.db')
 cursorObject = connectionObject.cursor()
@@ -7,9 +8,9 @@ cursorObject = connectionObject.cursor()
 class ContactBook:
     print('''
         Используйте название = ContactBook() для создания книги.
-        Используйте название.comands для просмора комманд.''')
+        Используйте название.instructions для просмора комманд.''')
 
-    def comands(self):
+    def myinstructions(self):
         print('''
 
         Используйте название.seeAllbook() для просмотра всех контактов.
@@ -17,7 +18,7 @@ class ContactBook:
         Используйте название.addnewContact('имя','адрес','номер телефона','эл. почта') для добавления контакта.
         Внимание! Все значения, указанные в скобках, должны быть в кавычках!
 
-        Используйте название.redactcon(порядковый номер контакта,порядковый номер данных,'новые данные')
+        Используйте название.redactbook(порядковый номер контакта,порядковый номер данных,'новые данные')
         для редактироания ваших контактов.
 
         Используйте название.deletecon(Порядковый номер контакта), чтобы удалить контакт.
@@ -53,16 +54,27 @@ class ContactBook:
                 for j in range(len(self.book[i])):
                     print(self.book[i][j])
 
-    def redactcon(self, contactNumber, propertyNumber, NewProperty):
+    def redactbook(self, contactNumber, propertyNumber, NewProperty):
         # Замена элемента в списке, исходя из введенных данных
         try:
+            if propertyNumber == 1:
+                name = self.book[contactNumber - 1][propertyNumber - 1]
+                newName = NewProperty
+                cursorObject.execute("UPDATE contacts SET name=? WHERE name=?", [name, newName])
+            elif propertyNumber == 2:
+                address = self.book[contactNumber - 1][propertyNumber - 1]
+                newAdress = NewProperty
+                cursorObject.execute("UPDATE contacts SET address=? WHERE address=?", [address, newAdress])
+            elif propertyNumber == 3:
+                phonenumber = self.book[contactNumber - 1][propertyNumber - 1]
+                newPhonenumber = NewProperty
+                cursorObject.execute("UPDATE contacts SET phonenumber=? WHERE phonenumber=?",
+                                     [phonenumber, newPhonenumber])
+            elif propertyNumber == 4:
+                mail = self.book[contactNumber - 1][propertyNumber - 1]
+                newMail = NewProperty
+                cursorObject.execute("UPDATE contacts SET mail=? WHERE mail=?", [mail, newMail])
             self.book[contactNumber - 1][propertyNumber - 1] = NewProperty
-            cursorObject.execute('DROP table contacts')
-            cursorObject.execute("create table if not exists contacts(name, address, phonenumber, mail)")
-            for i in range(len(self.book)):
-                data = (self.book[i][0], self.book[i][1], self.book[i][2], self.book[i][3])
-                cursorObject.execute('INSERT INTO contacts(name,address,phonenumber,mail) VALUES(?,?,?,?)', data)
-                data = ()
             connectionObject.commit()
             print('Данные были успешно изменены.')
         except IndexError:
@@ -72,14 +84,16 @@ class ContactBook:
         # Небольшая проверка на случай ошибочного ввода
         UserInput = int(input('Введите 1 для подтверждения или что угодно для отклонения.'))
         if UserInput == 1:
+            name = self.book[contactNumber - 1][0]
+            address = self.book[contactNumber - 1][1]
+            phonenumber = self.book[contactNumber - 1][2]
+            mail = self.book[contactNumber - 1][3]
+            cursorObject.execute("DELETE FROM contacts WHERE name=?", [name])
+            cursorObject.execute("DELETE FROM contacts WHERE address=?", [address])
+            cursorObject.execute("DELETE FROM contacts WHERE phonenumber=?", [phonenumber])
+            cursorObject.execute("DELETE FROM contacts WHERE mail=?", [mail])
             # Удаление через del
             del self.book[contactNumber - 1]
-            cursorObject.execute('DROP table contacts')
-            cursorObject.execute("create table if not exists contacts(name, address, phonenumber, mail)")
-            for i in range(len(self.book)):
-                data = (self.book[i][0], self.book[i][1], self.book[i][2], self.book[i][3])
-                cursorObject.execute('INSERT INTO contacts(name,address,phonenumber,mail) VALUES(?,?,?,?)', data)
-                data = ()
             connectionObject.commit()
             print('Контакт удален')
         else:
@@ -94,10 +108,6 @@ class ContactBook:
                 del self.book[-1]
             cursorObject.execute('DROP table contacts')
             cursorObject.execute("create table if not exists contacts(name, address, phonenumber, mail)")
-            for i in range(len(self.book)):
-                data = (self.book[i][0], self.book[i][1], self.book[i][2], self.book[i][3])
-                cursorObject.execute('INSERT INTO contacts(name,address,phonenumber,mail) VALUES(?,?,?,?)', data)
-                data = ()
             connectionObject.commit()
             print('Книга удалена')
         else:
@@ -117,11 +127,4 @@ class ContactBook:
                         buf = self.book[j + 1]
                         self.book[j + 1] = self.book[j]
                         self.book[j] = buf
-            cursorObject.execute('DROP table contacts')
-            cursorObject.execute("create table if not exists contacts(name, address, phonenumber, mail)")
-            for i in range(len(self.book)):
-                data = (self.book[i][0], self.book[i][1], self.book[i][2], self.book[i][3])
-                cursorObject.execute('INSERT INTO contacts(name,address,phonenumber,mail) VALUES(?,?,?,?)', data)
-                data = ()
-            connectionObject.commit()
             print('Проведена сортировка в порядке ASCII.')
